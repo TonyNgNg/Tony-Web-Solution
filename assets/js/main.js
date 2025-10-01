0e4a666f8556d56823b7bb125c356b2687268326
TonyNgNg <wienqueng.t@gmail.com>
2025-10-01 18:07:31 -0400

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('formMessage').textContent = "Thank you! We'll be in touch soon.";
-  this.reset();
-});
